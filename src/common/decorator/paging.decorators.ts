9de13b043a5896ef3e72bc2361dfe82d111e13f1
kyoongdev <9898junjun2@gmail.com>
2024-01-14 18:16:38 +0900

    feat(paging): paging decorator

---
+++ src/common/decorator/paging.decorators.ts
@@ -0,0 +1,15 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+import { PagingDTO } from '@/utils/pagination';
+
+export interface PagingQuery {
+  page: number;
+  limit: number;
+}
+
+export const Paging = createParamDecorator((_, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+
+  const { page, limit } = request.query;
+  return new PagingDTO(page ?? 0, limit ?? 20);
+});
