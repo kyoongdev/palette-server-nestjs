4bad1b0921c9ea2fa9952bc18c0f13bfd632464f
kyoongdev <9898junjun2@gmail.com>
2024-01-19 16:03:32 +0900

    feat(musician): musician repository

--- src/modules/musician/musician.repository.ts
+++ src/modules/musician/musician.repository.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
+import { Prisma } from '@prisma/client';
+
 import { CustomException } from '@/common/error/custom.exception';
 import { PrismaDatabase } from '@/database/prisma.repository';
 
@@ -21,5 +23,41 @@ export class MusicianRepository {
     if (!musician) {
       throw new CustomException(MUSICIAN_ERROR_CODE.MUSICIAN_NOT_FOUND);
     }
+
+    return musician;
+  }
+
+  async createMusician(data: Prisma.MusicianCreateArgs['data']) {
+    return await this.database.getRepository().musician.create({
+      data,
+    });
+  }
+
+  async updateMusician(id: string, data: Prisma.MusicianUpdateArgs['data']) {
+    await this.database.getRepository().musician.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+
+  async deleteMusician(id: string) {
+    await this.database.getRepository().musician.update({
+      where: {
+        id,
+      },
+      data: {
+        deletedAt: null,
+      },
+    });
+  }
+
+  async hardDeleteMusician(id: string) {
+    await this.database.getRepository().musician.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
