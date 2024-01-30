978b9499d6af55b7d1b72ff21ae1dd5ccf289feb
kyoongdev <9898junjun2@gmail.com>
2024-01-30 22:30:22 +0900

    feat(query): find artist list query nullable field

--- src/modules/services/artist/dto/query/find-artist-list.query.ts
+++ src/modules/services/artist/dto/query/find-artist-list.query.ts
@@ -6,7 +6,7 @@ import { Property } from '@/utils/swagger';
 type FindArtistSort = 'POPULARITY';
 
 export class FindArtistListQuery extends PagingDTO {
-  @Property({ apiProperty: { type: 'string', description: '판매 유형 id (전체는 undefined)' } })
+  @Property({ apiProperty: { type: 'string', description: '판매 유형 id (전체는 undefined)', nullable: true } })
   saleTypeId?: string;
 
   @Property({ apiProperty: { type: 'string', description: '순서', nullable: true, enum: ['POPULARITY'] } })
